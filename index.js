348501254b196b2e34cd84f1db4a69065cdc52ee
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-07-03 23:04:25 +0530

    Backend Init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("This is my first backend application running on NodeJS");
+const a = 5;
+const b = 10;
+const c = a+b;
+console.log("SUM",c);
+
+const arr = ["John","Jack", "Alex"];
+console.log("Hello from Nodemon",arr);
