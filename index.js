a9d56ee259d53e834cd6b17a51d7da7a7c497904
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-07-05 23:02:25 +0530

    Events and FS module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const os = require("node:os");
 const fs = require("node:fs");
+const events = require("node:events");
+const http = require("node:http");
 
 // console.log(os.arch());
 // console.log("A", os.EOL, "B");
@@ -26,28 +28,103 @@ const fs = require("node:fs");
 // console.log(os.constants);
 // console.clear();
 
+const fileName = "sample.txt";
 // CREATE OPERATION
-// fs.writeFile(filePath,fileContent,callbackFn);
-// fs.writeFile(
-//   "sample.txt",
-//   "Hi There!",
-//   (error) => {
-//     if (error) {
-//       console.log(error);
-//     }
-//   }
-// );
-
+const writeFile = () => {
+  // fs.writeFile(filePath,fileContent,callbackFn);
+  fs.writeFile(fileName, "Hi There!", (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
 
 const updateFile = () => {
-    // UPDATE/EDIT OPERATION
-    const msg = new Date().toLocaleString() + " User changed settings \n";
-    // fs.appendFile("sample.txt", msg, (error) => {
-    //   if (error) {
-    //     console.log(error);
-    //   }
-    // });
-    fs.appendFileSync("sample.txt",msg);
-}
+  // UPDATE/EDIT OPERATION
+  const msg = new Date().toLocaleString() + " User changed settings \n";
+  // fs.appendFile("sample.txt", msg, (error) => {
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  // });
+  fs.appendFileSync(fileName, msg);
+};
 
 // updateFile();
+
+const readFile = () => {
+  fs.readFile(fileName, (error, data) => {
+    if (error) {
+      console.log("ERROR WHILE READING THE FILE", error);
+      return;
+    }
+    console.log("FILE_CONTENT", data.toString());
+  });
+};
+
+// readFile();
+
+const deleteFile = () => {
+  fs.unlink(fileName, (err) => {
+    if (err) {
+      console.log("ERROR WHILE DELETING FILE", err);
+      return;
+    }
+    console.log("FILE", fileName, "DELETED SUCCESSFULLY");
+  });
+};
+
+// deleteFile();
+
+const createFolder = () => {
+  fs.mkdir("logs", (err) => {
+    if (err) {
+      console.log("ERROR CREATING FOLDER");
+      return;
+    }
+    console.log("FOLDER CREATED SUCCESSFULLY");
+  });
+};
+
+// createFolder();
+
+const folderExists = () => {
+  const exists = fs.existsSync("abcd");
+  console.log(exists);
+};
+
+// folderExists();
+
+// Events Module
+const myEmitter = new events.EventEmitter();
+const SIGNAL_CHANGE = "SIGNAL_CHANGE";
+
+// Listen to the event (Event Listener)
+myEmitter.on(SIGNAL_CHANGE, (signalColor) => {
+  const msg =
+    new Date().toLocaleString() + ": SIGNAL CHANGED TO " + signalColor + os.EOL;
+  fs.appendFile("signal.log", msg, (err) => {
+    if (err) {
+      console.log("ERROR WHILE WRITING LOG", err);
+      return;
+    }
+  });
+});
+
+const signalColors = ["GREEN", "YELLOW", "RED"];
+
+let counter = 0;
+// Emit an event (Event Emitter)
+// setInterval(() => {
+//   const index = counter % 3;
+//   counter++;
+//   //   console.log(signalColors[index]);
+//   myEmitter.emit(SIGNAL_CHANGE, signalColors[index]);
+// }, 30_000);
+
+// HTTP Module
+http
+  .createServer(() => {
+    console.log("SERVER IS UP AND RUNNING", os.cpus()[0].model);
+  })
+  .listen(8080);
