f413ac9700fa71bf402e39d6a1b6325fbbefb2d5
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-07-12 23:11:20 +0530

    Express apis

--- index.js
+++ index.js
@@ -1,172 +1,74 @@
-const os = require("node:os"); // CJS
-const fs = require("node:fs");
-const events = require("node:events");
-const http = require("node:http");
+const express = require("express");
+const app = express(); // http.createServer()
 
-const functions = require("./module.js");
-
-// const functions = require("divtestcalc");
-
-const sum = functions.add(5, 7);
-console.log(sum);
-
-const difference = functions.subtract(20, 2);
-console.log(difference);
-
-// console.log(os.arch());
-// console.log("A", os.EOL, "B");
-// console.log("CORES AVAILALBE IN THIS CPU", os.cpus()[1].speed);
-
-// const cpus = os.cpus();
-// for (let i = 0; i < cpus.length - 1; i++) {
-//   const core = cpus[i];
-//   console.log("SPEED OF CORE ", i + 1, " = ", core.speed);
-// }
-//
-// console.clear();
-// console.log(os.freemem());
-// console.log(os.homedir());
-// console.log(os.hostname());
-
-// console.log(os.networkInterfaces());
-// console.log(os.platform());
-// console.log(os.release());
-// console.log(os.tmpdir());
-// console.log(os.type());
-// console.log(os.uptime() / 3600);
-// console.log(os.userInfo());
-// console.log(os.constants);
-// console.clear();
-
-const fileName = "sample.txt";
-// CREATE OPERATION
-const writeFile = () => {
-  // fs.writeFile(filePath,fileContent,callbackFn);
-  fs.writeFile(fileName, "Hi There!", (error) => {
-    if (error) {
-      console.log(error);
-    }
-  });
-};
-
-const updateFile = () => {
-  // UPDATE/EDIT OPERATION
-  const msg = new Date().toLocaleString() + " User changed settings \n";
-  // fs.appendFile("sample.txt", msg, (error) => {
-  //   if (error) {
-  //     console.log(error);
-  //   }
-  // });
-  fs.appendFileSync(fileName, msg);
-};
-
-// updateFile();
-
-const readFile = () => {
-  fs.readFile(fileName, (error, data) => {
-    if (error) {
-      console.log("ERROR WHILE READING THE FILE", error);
-      return;
-    }
-    console.log("FILE_CONTENT", data.toString());
-  });
-};
-
-// readFile();
-
-const deleteFile = () => {
-  fs.unlink(fileName, (err) => {
-    if (err) {
-      console.log("ERROR WHILE DELETING FILE", err);
-      return;
-    }
-    console.log("FILE", fileName, "DELETED SUCCESSFULLY");
-  });
-};
-
-// deleteFile();
-
-const createFolder = () => {
-  fs.mkdir("logs", (err) => {
-    if (err) {
-      console.log("ERROR CREATING FOLDER");
-      return;
-    }
-    console.log("FOLDER CREATED SUCCESSFULLY");
-  });
-};
-
-// createFolder();
-
-const folderExists = () => {
-  const exists = fs.existsSync("abcd");
-  console.log(exists);
-};
-
-// folderExists();
-
-// Events Module
-const myEmitter = new events.EventEmitter();
-const SIGNAL_CHANGE = "SIGNAL_CHANGE";
-
-// Listen to the event (Event Listener)
-myEmitter.on(SIGNAL_CHANGE, (signalColor) => {
-  const msg =
-    new Date().toLocaleString() + ": SIGNAL CHANGED TO " + signalColor + os.EOL;
-  fs.appendFile("signal.log", msg, (err) => {
-    if (err) {
-      console.log("ERROR WHILE WRITING LOG", err);
-      return;
-    }
-  });
+app.get("/", (req, res) => {
+  res.send("The express server is up and running");
 });
 
-const signalColors = ["GREEN", "YELLOW", "RED"];
+app.get("/todos", (req, res) => {
+  const todo = {
+    id: 1,
+    title: "Lorem Ipsum",
+    description: "aljsdfl kajsdflkjasdlf jasdfl kjasdflk",
+    completed: false,
+  };
+  res.json(todo);
+});
 
-let counter = 0;
-// Emit an event (Event Emitter)
-// setInterval(() => {
-//   const index = counter % 3;
-//   counter++;
-//   //   console.log(signalColors[index]);
-//   myEmitter.emit(SIGNAL_CHANGE, signalColors[index]);
-// }, 30_000);
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params.userId);
+  const user = {
+    userId: req.params.userId,
+  };
 
-// Sample JSON data
-const todos = [
-  { id: 1, title: "lomre ipsum", completed: true },
-  { id: 2, title: "lomre ipsum1", completed: true },
-  { id: 3, title: "lomre ipsum2", completed: false },
-  { id: 4, title: "lomre ipsum3", completed: false },
-  { id: 5, title: "lomre ipsum4", completed: true },
-  { id: 6, title: "lomre ipsum5", completed: true },
-  { id: 7, title: "lomre ipsum6", completed: false },
+  res.json(user);
+});
+
+app.post("/user", (req, res) => {
+  const responseJson = {
+    sucees: true,
+    message: "User created successfully",
+  };
+
+  res.json(responseJson);
+});
+
+app.put("/user", (req, res) => {
+  const putResponse = {
+    success: true,
+    message: "User replaced successfully",
+  };
+
+  res.json(putResponse);
+});
+
+app.delete("/user", (req, res) => {
+  const deleteResponse = {
+    success: true,
+    message: "User deleted successfully",
+  };
+
+  res.json(deleteResponse);
+});
+
+const products = [
+  { id: 1, name: "Product Name", price: 2000, quantity: 50 },
+  { id: 2, name: "Product Name", price: 2000, quantity: 50 },
+  { id: 3, name: "Product Name", price: 2000, quantity: 50 },
 ];
 
-const todosString = JSON.stringify(todos);
-
-// API ROUTE
-const server = (req, res) => {
-  console.log("URL", req.method);
-  if (req.url === "/todos") {
-    // Endpoint
-    if (req.method === "GET") {
-      // res.write("To do list will appear here, this is a GET request");
-      res.end(todosString);
-    } else if (req.method === "POST") {
-      res.write("This route will be used to create a new ToDo");
-    }
-  } else if (req.url === "/posts") {
-    res.write("Posts list will appear here");
+app.get("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id == productId);
+  if (product) {
+    res.json(product);
   } else {
-    res.write(`Error 404, request route ${req.url} not found`);
+    res.status(404).json({ message: "Product not found" });
   }
-  res.end(); // Server sends the response back to the client
-};
+});
 
-const onServerUp = () => {
-  console.log("Server is up and running on port 8080");
-};
-
-// HTTP Module
-// http.createServer(server).listen(8080, onServerUp);
+const port = 5000;
+app.listen(port, () => {
+  //server.listen()
+  console.log("Server is up and running on port", port);
+});
